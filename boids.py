2fb9ad31f57cbeb8543ff22a60cfff75a835e1fe
Mark Graham <markgraham539@gmail.com>
2014-11-13 14:54:25 +0000

    Changed number of boids from a raw number appearing in the code to a variable

--- boids.py
+++ boids.py
@@ -8,34 +8,35 @@ from matplotlib import animation
 import random
 
 # Deliberately terrible code for teaching purposes
+numberOfBoids = 50
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+boids_x=[random.uniform(-450,50.0) for x in range(numberOfBoids)]
+boids_y=[random.uniform(300.0,600.0) for x in range(numberOfBoids)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(numberOfBoids)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(numberOfBoids)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
 	xs,ys,xvs,yvs=boids
 	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+	for i in range(numberOfBoids):
+		for j in range(numberOfBoids):
+			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/numberOfBoids
+			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/numberOfBoids
 	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
+	for i in range(numberOfBoids):
+		for j in range(numberOfBoids):
 			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
 				xvs[i]=xvs[i]+(xs[i]-xs[j])
 				yvs[i]=yvs[i]+(ys[i]-ys[j])
 	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
+	for i in range(numberOfBoids):
+		for j in range(numberOfBoids):
 			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/numberOfBoids
+				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/numberOfBoids
 	# Move according to velocities
-	for i in range(len(xs)):
+	for i in range(numberOfBoids):
 		xs[i]=xs[i]+xvs[i]
 		ys[i]=ys[i]+yvs[i]
 
